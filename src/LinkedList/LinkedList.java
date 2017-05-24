088ad82a28306ad8cd4ef8c74258402d131466cd
Jonathan Mooneyham <mooneyj3@wwu.edu>
2017-05-23 18:40:39 -0700

    Initial commit

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+/**
+ * Created by J-moneyham on 5/23/17.
+ * Singly Linked List that stores integers in each node
+ */
+public class LinkedList {
+
+    Node first;
+    
+
+    public LinkedList() {
+
+    }
+
+
+
+}
---
+++ src/LinkedList/Node.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+/**
+ * Created by J-moneyham on 5/23/17.
+ */
+public class Node {
+
+    private int number;
+    private Node next;
+
+
+    public Node() {
+        number = 0;
+        next = null;
+    }
+
+    public Node(int number, Node next) {
+        this.number = number;
+        this.next = next;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+}
